ee1bb653b3aa081cc8a7f47da6e4f53dbf400fcd
Simon Egli <eglisi1@students.zhaw.ch>
2023-04-25 15:28:59 +0200

    create project with initializr

---
+++ src/main/java/ch/zhaw/eglisi/deepjavalibraryinference/DeepJavaLibraryInferenceApplication.java
@@ -0,0 +1,13 @@
+package ch.zhaw.eglisi.deepjavalibraryinference;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepJavaLibraryInferenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeepJavaLibraryInferenceApplication.class, args);
+    }
+
+}
